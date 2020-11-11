e6547f83667d2cc5acfe19bde0d682b0fd444ddd
FilipsFedotovs <74325502+FilipsFedotovs@users.noreply.github.com>
2020-11-11 22:15:22 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -9,12 +9,19 @@ import subprocess
 CurrDir=os.getcwd()
 print 'Current directory is set as:', os.getcwd()
 #Writing this directory into csv file so the software knows where to look data/code
-csv_writer=open(CurrDir+'DirMap',"w")
+csv_writer=open('DirMap',"w")
 dir_writer = csv.writer(csv_writer)
 string_to_write=[]
 string_to_write.append('AFS_DIR')
 string_to_write.append(CurrDir)
 dir_writer.writerow(string_to_write)
+print 'Created the directory mapping file'
+
+########################################     Create directories for HTCondor    #########################################
+os.mkdir('HTCondor/MSG')
+os.mkdir('HTCondor/SUB')
+os.mkdir('HTCondor/SH')
+print 'HTCondor folders have been created:'
 
 exit()
 
