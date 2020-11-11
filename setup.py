e84b6db40aa43dc71e458b8fafc19c562eb1a4f9
FilipsFedotovs <74325502+FilipsFedotovs@users.noreply.github.com>
2020-11-11 21:52:50 +0000

    Add files via upload
    Uploading first setup file template

---
+++ setup.py
@@ -0,0 +1,18 @@
+#Belogs to: EDER-TRAN
+#Purpose: To setup all working directories for the new user
+
+########################################    Import libraries    #############################################
+import csv
+import os, shutil
+import subprocess
+########################################     Work out the current directory    #########################################
+
+print 'Current directory is:', getcwd()
+
+exit()
+
+
+
+
+
+
