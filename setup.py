f36b952edbe2cd40599291c91a6f768874b9d20d
FilipsFedotovs <74325502+FilipsFedotovs@users.noreply.github.com>
2020-11-11 22:04:40 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,9 +5,16 @@
 import csv
 import os, shutil
 import subprocess
-########################################     Work out the current directory    #########################################
-
-print 'Current directory is:', os.getcwd()
+########################################     Work out and registering the current directory    #########################################
+CurrDir=os.getcwd()
+print 'Current directory is set as:', os.getcwd()
+#Writing this directory into csv file so the software knows where to look data/code
+csv_writer=open(CurrDir+'DirMap',"w")
+dir_writer = csv.writer(csv_writer)
+string_to_write=[]
+string_to_write.append('AFS_DIR')
+string_to_write.append(CurrDir)
+dir_writer.writerow(string_to_write)
 
 exit()
 
